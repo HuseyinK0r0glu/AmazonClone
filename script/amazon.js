89247c350ab5bdad0879e1494dd65ebb5dc4cd50
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-19 08:13:25 +0300

    use of product.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,29 +1,4 @@
-const products = [{
-    image : "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating : {
-        stars : 4.5,
-        count : 87
-    },
-    // javascript has problems with calculating the decimals so we store the price as cents
-    priceCents : 1090
-},{
-    image : "images/products/intermediate-composite-basketball.jpg",
-    name : 'Intermediate Size Basketball',
-    rating : {
-        stars : 4,
-        count : 127
-    },
-    priceCents : 2095
-},{
-    image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating : {
-        stars : 4.5,
-        count : 56,
-    },
-    priceCents : 799
-}]; 
+// products are coming from the array inside of the product.js insdide of the data folder
 
 let productsHTML = '';     
 
@@ -51,7 +26,7 @@ products.forEach((product) => {
             $${(product.priceCents / 100).toFixed(2)}
           </div>
 
-          <div class="product-quantity-container">
+          <div class="product-quanti ty-container">
             <select>
               <option selected value="1">1</option>
               <option value="2">2</option>
